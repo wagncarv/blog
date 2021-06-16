5a79a35d85f2956255146ac546ebecafebc760b4
Gustavo <g.92oliveira@gmail.com>
2021-06-16 16:50:14 -0300

    fix: coveralls.json

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule Blog.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.json": :test
       ]
     ]
   end
