e5c2c484e2f1657705fc83b8da0233a6f19705b4
wagner <wagncarv@gmail.com>
2022-01-23 18:09:45 -0300

    deploy gigalixir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Blog.MixProject do
     [
       app: :blog,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       description: "Projeto para aprender fundamentos do Phoenix com Elixir",
       source_url: @github_url,
       homepage_url: @github_url,
